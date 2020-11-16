a9026645a6d4b92fe1082b1ba7fcffaa79fa7ee4
imvishalnag <imvishalnag@gmail.com>
2020-11-16 16:48:38 +0530

    receit added

---
+++ print.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Santirekha</title>
+  <style>.text-center{text-align:center}.text-right{text-align:right}.service td:last-child,.payment,.Rate{text-align:right;padding-left:0;padding-right:10px}body{margin:0;background:#5d5858}#invoice-POS{padding:2mm;width:3in;background:#FFF}#invoice-POS ::selection{background:#f31544;color:#FFF}#invoice-POS ::moz-selection{background:#f31544;color:#FFF}#invoice-POS h1{font-size:1.5em;color:#222}#invoice-POS h2{font-size:.9em;margin:0}#invoice-POS h3{font-size:1.2em;font-weight:300;line-height:2em}#invoice-POS p{font-size:.7em;color:#666;line-height:1.2em;margin:0}#invoice-POS #mid{border-bottom:1px solid #EEE}#invoice-POS #mid{min-height:80px}#invoice-POS #bot{min-height:50px}#invoice-POS #top .logo img{width:100%}#invoice-POS .clientlogo{float:left;height:60px;width:60px;background:url(../../uploads/santirekhalogo.png) no-repeat;background-size:60px 60px;border-radius:50px}#invoice-POS .info{display:block;margin-bottom:5px;border:1px solid #eee;padding:5px}#invoice-POS .title{float:right}#invoice-POS .title p{text-align:right}#invoice-POS table{width:100%;border-collapse:collapse}#invoice-POS .tabletitle{font-size:.5em;border-bottom:1px solid #eee}#invoice-POS .service{border-bottom:1px solid #EEE}#invoice-POS .item{width:24mm}#invoice-POS .itemtext{font-size:.6em}#invoice-POS #legalcopy{margin-top:2mm;border-top:1px solid #ddd;padding-top:1mm}table tr td{padding:5px 0 5px 10px;border-left:1px solid #eee}.font-700{font-weight:700}#table h2{font-size:11px}table .tabletitle:first-child td{border-bottom:1px solid #eee}#table .botton-tail h2{font-size:10px}#table .botton-tail td{padding-top:1.5px;padding-bottom:1.5px;border-right:1px solid #eee}.top-table tr td{padding:0;font-size:10px}</style>
+
+</head>
+<body>
+	<!-- partial:index.partial.html -->
+	<div id="invoice-POS">		
+		<div>
+			<table class="top-table">
+				<tr>
+					<td><strong>Order Date</strong> : 12/10/2001</td>
+					<td class="text-right"><strong>Order ID</strong> : JK01235GHU</td>
+				</tr>
+			</table>
+		</div>
+		<div class="text-center" id="top">
+			<div class="logo"><img src="uploads/santirekhalogo.png" alt=""></div>
+		</div><!--End InvoiceTop-->
+		
+		<div id="mid">
+			<div class="info">
+				<h2>Billing Info</h2>
+				<p> 
+					Vishal Nag</br>
+				</p>
+			</div>
+			<div class="info">
+				<h2>Shipping Info</h2>
+				<p> 
+					Address : street city, state 0000</br>
+					Email   : JohnDoe@gmail.com</br>
+					Phone   : 555-555-5555</br>
+				</p>
+			</div>
+		</div><!--End Invoice Mid-->
+		
+		<div id="bot">
+			<div id="table">
+				<table>
+					<tr class="tabletitle">
+						<td class="item"><h2>Item</h2></td>
+						<td class="Hours"><h2>Sale Price</h2></td>
+						<td class="Hours"><h2>Qty</h2></td>
+						<td class="Hours"><h2>Save</h2></td>
+						<td class="Rate"><h2>Amount</h2></td>
+					</tr>
+
+					<tr class="service">
+						<td class="tableitem"><p class="itemtext"><span class="font-700">Communication</span> <br>MRP ₹555</p></td>
+						<td class="tableitem text-center"><p class="itemtext">5</p></td>					
+						<td class="tableitem text-center"><p class="itemtext">5</p></td>				
+						<td class="tableitem text-center"><p class="itemtext">5</p></td>
+						<td class="tableitem"><p class="itemtext">$375.00</p></td>
+					</tr>
+
+					<tr class="service">
+						<td class="tableitem"><p class="itemtext"><span class="font-700">Asset Gathering</span> <br>MRP ₹555</p></td>
+						<td class="tableitem text-center"><p class="itemtext">3</p></td>					
+						<td class="tableitem text-center"><p class="itemtext">3</p></td>				
+						<td class="tableitem text-center"><p class="itemtext">5</p></td>
+						<td class="tableitem"><p class="itemtext">$225.00</p></td>
+					</tr>
+
+					<tr class="service">
+						<td class="tableitem"><p class="itemtext"><span class="font-700">Design Development</span> <br>MRP ₹555</p></td>
+						<td class="tableitem text-center"><p class="itemtext">5</p></td>					
+						<td class="tableitem text-center"><p class="itemtext">5</p></td>				
+						<td class="tableitem text-center"><p class="itemtext">5</p></td>
+						<td class="tableitem"><p class="itemtext">$375.00</p></td>
+					</tr>
+
+					<tr class="service">
+						<td class="tableitem"><p class="itemtext"><span class="font-700">Animation</span> <br>MRP ₹555</p></td>
+						<td class="tableitem text-center"><p class="itemtext">20</p></td>						
+						<td class="tableitem text-center"><p class="itemtext">20</p></td>				
+						<td class="tableitem text-center"><p class="itemtext">5</p></td>
+						<td class="tableitem"><p class="itemtext">$1500.00</p></td>
+					</tr>
+
+					<tr class="service">
+						<td class="tableitem"><p class="itemtext"><span class="font-700">Animation Revisions</span> <br>MRP ₹555</p></td>
+						<td class="tableitem text-center"><p class="itemtext">10</p></td>						
+						<td class="tableitem text-center"><p class="itemtext">10</p></td>				
+						<td class="tableitem text-center"><p class="itemtext">5</p></td>
+						<td class="tableitem"><p class="itemtext">$750.00</p></td>
+					</tr>
+
+
+					<tr class="tabletitle botton-tail">
+						<td class="Rate text-right" colspan=3><h2>Free shopping</h2></td>
+						<td class="payment"><h2>$419.25</h2></td>
+						<td class="payment"><h2>$419.25</h2></td>
+					</tr>
+					<tr class="tabletitle botton-tail">
+						<td class="Rate text-right" colspan=4><h2>Free shopping</h2></td>
+						<td class="payment"><h2>$419.25</h2></td>
+					</tr>
+					<tr class="tabletitle botton-tail">
+						<td class="Rate text-right" colspan=4><h2>Net total</h2></td>
+						<td class="payment"><h2>$3,644.25</h2></td>
+					</tr>
+					<tr class="tabletitle botton-tail">
+						<td class="Rate text-right" colspan=4><h2>Previous balance </h2></td>
+						<td class="payment"><h2>$419.25</h2></td>
+					</tr>
+					<tr class="tabletitle botton-tail">
+						<td class="Rate text-right" colspan=4><h2>Payment Amount  </h2></td>
+						<td class="payment"><h2>$419.25</h2></td>
+					</tr>
+					<tr class="tabletitle botton-tail">
+						<td class="Rate text-right" colspan=4><h2>Service Charge</h2></td>
+						<td class="payment"><h2>$419.25</h2></td>
+					</tr>  
+					<tr class="tabletitle botton-tail">
+						<td class="Rate text-right" colspan=4><h2>Gross Amount</h2></td>
+						<td class="payment"><h2>$419.25</h2></td>
+					</tr>  
+					<tr class="tabletitle botton-tail">
+						<td class="Rate text-right" colspan=4><h2>Cash payment </h2></td>
+						<td class="payment"><h2>$419.25</h2></td>
+					</tr>  
+					<tr class="tabletitle botton-tail">
+						<td class="Rate text-right" colspan=4><h2>Balance</h2></td>
+						<td class="payment"><h2>$419.25</h2></td>
+					</tr>  
+				</table>
+			</div><!--End Table-->
+
+			<div id="legalcopy">
+				<p class="legal text-center"><strong>Thank you for your business!</strong> </p>
+				<p class="legal">If you have any query mail us on info@santirekha.com or call us on +91 98745 562310 </p>
+			</div>
+		</div><!--End InvoiceBot-->
+	</div><!--End Invoice-->
+	<!-- partial -->
+  
+</body>
+</html>
